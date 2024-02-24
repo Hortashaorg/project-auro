d12056959019349184c37553bd55ff47935ec65f
Hortasha <eidemartin_303@hotmail.com>
2024-02-24 08:35:24 +0000

    astro service

--- services/astro.config.mjs
+++ services/astro.config.mjs
@@ -5,9 +5,9 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    adapter: node({
-      mode: 'standalone',
-    }),
-    integrations: [svelte(), tailwind()],
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
+  integrations: [svelte(), tailwind()]
 });
