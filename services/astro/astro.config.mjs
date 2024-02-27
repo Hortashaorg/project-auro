c429906ff82ea6109a29de64b471dd509bb72c27
Hortasha <eidemartin_303@hotmail.com>
2024-02-27 19:57:57 +0000

    everything needed for auth of users through google cloud seem to be working fine

--- services/astro/astro.config.mjs
+++ services/astro/astro.config.mjs
@@ -1,6 +1,7 @@
 import node from "@astrojs/node";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
@@ -10,4 +11,10 @@ export default defineConfig({
     mode: "standalone",
   }),
   integrations: [svelte(), tailwind()],
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
 });
