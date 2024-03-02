c81cf5b7a3cf8982be997144d1599f4d657c3cff
Hortasha <eidemartin_303@hotmail.com>
2024-03-02 12:16:24 +0000

    fix linting and redirect endpoint for authentication

--- packages/database/src/backend/db/database-schema.d.ts
+++ packages/database/src/backend/db/database-schema.d.ts
@@ -1,14 +1,26 @@
 import type { ColumnType } from "kysely";
 
-export type Assettype = "assignment" | "character" | "item" | "resource" | "skill";
+export type Assettype =
+  | "assignment"
+  | "character"
+  | "item"
+  | "resource"
+  | "skill";
 
 export type Droptype = "character" | "item" | "resource" | "skill";
 
-export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
-  ? ColumnType<S, I | undefined, U>
-  : ColumnType<T, T | undefined, T>;
+export type Generated<T> =
+  T extends ColumnType<infer S, infer I, infer U>
+    ? ColumnType<S, I | undefined, U>
+    : ColumnType<T, T | undefined, T>;
 
-export type Rarity = "ancient" | "common" | "epic" | "legendary" | "rare" | "uncommon";
+export type Rarity =
+  | "ancient"
+  | "common"
+  | "epic"
+  | "legendary"
+  | "rare"
+  | "uncommon";
 
 export type Startingtype = "character" | "item" | "resource";
 
