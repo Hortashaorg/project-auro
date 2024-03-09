12826eb948813291e28bd51884d5765b961d282a
Hortasha <eidemartin_303@hotmail.com>
2024-03-09 12:30:12 +0000

    add the button in a fragment

--- services/astro/src/components/Wrapper.tsx
+++ services/astro/src/components/Wrapper.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 // Define the component using a functional approach
 export const Wrapper: React.FC = () => (
-  <div>
+  <>
     <Button onClick={() => console.log("hello world")}>Hello world</Button>
-  </div>
+  </>
 );
