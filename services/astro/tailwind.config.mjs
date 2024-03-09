d482b02d71c0b56d028352fbcb65f4d19601a721
Hortasha <eidemartin_303@hotmail.com>
2024-03-09 12:08:05 +0000

    shadcn/ui should now work fine in the project

--- services/astro/tailwind.config.mjs
+++ services/astro/tailwind.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],,
-  prefix: "",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     container: {
       center: true,
@@ -68,5 +67,6 @@ module.exports = {
       },
     },
   },
+  // eslint-disable-next-line global-require
   plugins: [require("tailwindcss-animate")],
 };
