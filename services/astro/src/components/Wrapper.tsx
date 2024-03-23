7ac362b4fd739140d9d988bad420d961d730eff4
Hortasha <eidemartin_303@hotmail.com>
2024-03-23 14:38:22 +0000

    getting started making a nav menu

--- services/astro/src/components/Wrapper.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@comp/shadui/ui/button";
-import React from "react";
-
-// Define the component using a functional approach
-export const Wrapper: React.FC = () => (
-  <>
-    <Button onClick={() => console.log("hello world")}>Hello world</Button>
-  </>
-);
