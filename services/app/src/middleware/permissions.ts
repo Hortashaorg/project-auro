7c039296d1a007f741e736a7ad26b487d3aeda44
Hortasha <eidemartin_303@hotmail.com>
2024-07-09 09:40:59 +0000

    fix permissions

--- services/app/src/middleware/permissions.ts
+++ services/app/src/middleware/permissions.ts
@@ -1,20 +1,40 @@
 import type { APIContext } from "astro";
 
 const isLoggedIn = ({ locals }: APIContext) => locals.loggedIn;
+const isPublic = () => true;
 
 const paths = [
 	{
 		url: "/profile",
 		permission: isLoggedIn,
 	},
+	{
+		url: "/api",
+		permission: isLoggedIn,
+	},
+	{
+		url: "/",
+		permission: isPublic,
+	},
+	{
+		url: "/auth",
+		permission: isPublic,
+	},
 ];
 
 export const hasAccessToUrl = (context: APIContext) => {
-	const foundPath = paths.find((p) => p.url === context.url.pathname);
-	if (foundPath) {
-		return foundPath.permission(context);
+	const foundPaths = paths.filter((p) =>
+		context.url.pathname.startsWith(p.url),
+	);
+	if (foundPaths.length > 0) {
+		for (const path of foundPaths) {
+			if (!path.permission(context)) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	// If there is no permission set for the path, we assume it's public
-	return true;
+	return false;
 };
