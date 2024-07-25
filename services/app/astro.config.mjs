c1eebbcfe85d91b7cfd6b8e326182a1dccbd8c52
Hortasha <eidemartin_303@hotmail.com>
2024-07-25 09:13:38 +0000

    upgrade packages and button tweak

--- services/app/astro.config.mjs
+++ services/app/astro.config.mjs
@@ -1,21 +1,21 @@
+import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
 
 import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [basicSsl(), tailwind(), alpinejs()],
-  output: "server",
-  adapter: node({
-    mode: "standalone"
-  }),
-  vite: {
-    plugins: [basicSsl()],
-    server: {
-      https: true
-    }
-  }
+	integrations: [basicSsl(), tailwind(), alpinejs()],
+	output: "server",
+	adapter: node({
+		mode: "standalone",
+	}),
+	vite: {
+		plugins: [basicSsl()],
+		server: {
+			https: true,
+		},
+	},
 });
